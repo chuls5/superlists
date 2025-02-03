525c6bcb78a07a64c938a47234ee78b9a99ef6d3
Cody Huls <cody.harrison.huls@gmail.com>
2025-02-02 20:03:41 -0700

    Refactor list handling: Separate list view, update URLs, and modify template structure

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,6 @@ from django.urls import re_path  # Use re_path instead of url
 from lists import views
 
 urlpatterns = [
-    re_path(r"^$", views.home_page, name="home"),  # Replace url with re_path
+    re_path(r"^$", views.home_page, name="home"),
+    re_path(r"^lists/the-only-list-in-the-world/$", views.view_list, name='view_list'),
 ]
